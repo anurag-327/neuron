297221b6ced150e9ba5d7b1bafbfae08bb911353
Anurag <anuragsrivastav0027@gmail.com>
2025-12-03 01:51:22 +0530

    fix: Added langauage check on languages supported in create job api

---
+++ config/lang.go
@@ -0,0 +1,3 @@
+package config
+
+var SupportedLanguages = []string{"cpp", "python", "js", "java"}
--- internal/handler/handler.go
+++ internal/handler/handler.go
@@ -3,8 +3,10 @@ package handler
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
+	"github.com/anurag-327/neuron/config"
 	"github.com/anurag-327/neuron/internal/dto"
 	"github.com/anurag-327/neuron/internal/factory"
 	"github.com/anurag-327/neuron/internal/models"
@@ -19,13 +21,27 @@ func SubmitCodeHandler(c *gin.Context) {
 	ctx := c.Request.Context()
 	var body dto.SubmitCodeBody
 	if err := c.ShouldBindJSON(&body); err != nil {
-		response.Error(c, 400, err.Error())
+		response.Error(c, http.StatusBadRequest, err.Error())
 		return
 	}
+
+	langSupported := false
+	for _, lang := range config.SupportedLanguages {
+		if lang == body.Language {
+			langSupported = true
+			break
+		}
+	}
+
+	if !langSupported {
+		response.Error(c, http.StatusUnauthorized, "language not supported")
+		return
+	}
+
 	if body.Language == "cpp" {
 		// Checks on code blocks
 		if err := util.ValidateAndSanitizeCpp(body.Code); err != nil {
-			response.Error(c, 400, err.Error())
+			response.Error(c, http.StatusUnauthorized, err.Error())
 			return
 		}
 	}
@@ -40,38 +56,38 @@ func SubmitCodeHandler(c *gin.Context) {
 	}
 	job, err := repository.SaveJob(ctx, job)
 	if err != nil {
-		response.Error(c, 500, err.Error())
+		response.Error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Publish it to kafka queue
 	jobBytes, err := json.Marshal(job)
 	if err != nil {
-		response.Error(c, 500, err.Error())
+		response.Error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 	p, err := factory.GetPublisher()
 	if err != nil {
-		response.Error(c, 500, "Internal Server Error")
+		response.Error(c, http.StatusInternalServerError, "Internal Server Error")
 	}
 	if err := p.Publish("code-jobs", job.Language, jobBytes); err != nil {
 		_ = repository.DeleteJob(ctx, job)
-		response.Error(c, 500, err.Error())
+		response.Error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	response.Success(c, 200, "job queued successfully", gin.H{"jobId": job.ID, "status": job.Status})
+	response.Success(c, http.StatusOK, "job queued successfully", gin.H{"jobId": job.ID, "status": job.Status})
 }
 func GetJobStatusHandler(c *gin.Context) {
 	jobID := c.Param("jobId")
 	if jobID == "" {
-		response.Error(c, 400, "jobId is required")
+		response.Error(c, http.StatusBadRequest, "jobId is required")
 		return
 	}
 
 	_, err := util.IsValidObjectID(jobID)
 	if err != nil {
-		response.Error(c, 401, "Invalid Job ID")
+		response.Error(c, http.StatusBadRequest, "Invalid Job ID")
 		return
 	}
 
@@ -80,13 +96,13 @@ func GetJobStatusHandler(c *gin.Context) {
 	job, err := repository.GetJobByID(ctx, jobID)
 	fmt.Println(job, err)
 	if err != nil {
-		response.Error(c, 500, err.Error())
+		response.Error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Return minimal info for queued or running jobs
 	if job.Status == models.StatusQueued || job.Status == models.StatusRunning {
-		response.Success(c, 200, "status fetched successfully", gin.H{
+		response.Success(c, http.StatusOK, "status fetched successfully", gin.H{
 			"status": job.Status,
 			"jobId":  job.ID,
 		})
@@ -97,7 +113,7 @@ func GetJobStatusHandler(c *gin.Context) {
 	queueTime := job.StartedAt.Sub(job.QueuedAt)
 	totalTime := job.FinishedAt.Sub(job.QueuedAt)
 
-	response.Success(c, 200, "job result fetched successfully", gin.H{
+	response.Success(c, http.StatusOK, "job result fetched successfully", gin.H{
 		"jobId":               job.ID,
 		"status":              job.Status,
 		"stdout":              job.Stdout,
